5f1e62cbeea282b026aa90494463a5e06f5faf0a
dangnguyen146 <19521317@gm.uit.edu.vn>
2023-05-16 23:30:13 +0700

    fix swagger

--- src/main/java/com/prettyshopbe/prettyshopbe/PrettyshopbeApplication.java
+++ src/main/java/com/prettyshopbe/prettyshopbe/PrettyshopbeApplication.java
@@ -1,8 +1,12 @@
 package com.prettyshopbe.prettyshopbe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class PrettyshopbeApplication {
 
---
+++ src/main/java/com/prettyshopbe/prettyshopbe/model/ProductSize.java
@@ -0,0 +1,27 @@
+package com.prettyshopbe.prettyshopbe.model;
+
+public class ProductSize {
+    private String size;
+    private int quantity;
+
+    public ProductSize(String size, int quantity) {
+        this.size = size;
+        this.quantity = quantity;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
